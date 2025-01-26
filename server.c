576cd333be25b52fb5cae9398dd1dae0a0f45d70
Beatriz Fiochi Soares da Silva <bfiochi-@c1r8s5.42porto.com>
2025-01-26 19:33:19 +0000

    Changing signal to sigaction, using siginfo

--- server.c
+++ server.c
@@ -6,18 +6,20 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 19:40:11 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/01/26 15:01:01 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/01/26 19:31:52 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include <signal.h>
 
-void	handler(int signal)
+void	handler(int signal, siginfo_t *info, void *context)
 {
 	static int				i = 0;
 	static unsigned char	c = 0;
 
+	(void) context;
+	(void) info;
 	if(signal == SIGUSR2)
 		c = c << 1;
 	else if(signal == SIGUSR1)
@@ -30,12 +32,17 @@ void	handler(int signal)
 		c = 0;
 	}
 }
+//sigaction equivalente ao signal com adição do siginfo.
 
 int	main(void)
 {
+	struct sigaction	act;
+
 	ft_printf("PID: %d\n", getpid());
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
+	act.sa_flags = SA_RESTART | SA_SIGINFO;
+	act.sa_sigaction = handler;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
 	while (1)
 		pause();
 	return (0);
