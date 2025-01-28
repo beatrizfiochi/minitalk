c4b66b80e20da9dcbfe98a75dafcfdea9fc61c81
Beatriz Fiochi Soares da Silva <bfiochi-@c1r8s5.42porto.com>
2025-01-28 20:08:37 +0000

    Updating with server acknologe

--- client.c
+++ client.c
@@ -6,38 +6,62 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 21:40:27 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/01/26 15:42:32 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/01/28 19:50:33 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include <signal.h>
+#include <stdbool.h>
+#include <bits/types/siginfo_t.h>
+
+static volatile bool g_received;
 
 void	signal_sender(int pid, char *str)
 {
 	int	i;
 	int	c;
 
-	i = 0;
 	while(*str)
 	{
 		c = *(str);
+		i = 0;
 		while(i < 8)
 		{
+			g_received = false;
 			if((c << i) & 0b10000000)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
 			i++;
-			usleep(1000);
+			while (g_received ==  false)
+				pause();
 		}
 		str++;
-		i = 0;
 	}
+	i = 0;
+	while(i < 8)
+	{
+		g_received = false;
+		kill(pid, SIGUSR2);
+		i++;
+		while (g_received == false)
+			pause();
+	}
+}
+
+void	handler(int signal)
+{
+	(void) signal;
+	g_received = true;
 }
 
 int	main(int argc, char **argv)
 {
+	struct sigaction	act;
+
+	act.sa_handler = handler;
+	act.sa_flags = SA_SIGINFO;
 	if(argc != 3)
 	{
 		ft_printf("Error. Invalid number of arguments.\n");
@@ -45,6 +69,9 @@ int	main(int argc, char **argv)
 		exit(1);
 	}
 	else
+	{
+		sigaction(SIGUSR1, &act, NULL);
 		signal_sender(ft_atoi(argv[1]), argv[2]);
+	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,12 +6,13 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 19:40:11 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/01/26 19:31:52 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/01/28 19:51:07 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include <signal.h>
+#include <bits/types/siginfo_t.h>
 
 void	handler(int signal, siginfo_t *info, void *context)
 {
@@ -19,7 +20,6 @@ void	handler(int signal, siginfo_t *info, void *context)
 	static unsigned char	c = 0;
 
 	(void) context;
-	(void) info;
 	if(signal == SIGUSR2)
 		c = c << 1;
 	else if(signal == SIGUSR1)
@@ -27,19 +27,22 @@ void	handler(int signal, siginfo_t *info, void *context)
 	i++;
 	if(i == 8)
 	{
-		ft_printf("%c", c);
+		if(c == '\0')
+			ft_printf("\n");
+		else
+			ft_printf("%c", c);
 		i = 0;
 		c = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
 }
-//sigaction equivalente ao signal com adição do siginfo.
 
 int	main(void)
 {
 	struct sigaction	act;
 
 	ft_printf("PID: %d\n", getpid());
-	act.sa_flags = SA_RESTART | SA_SIGINFO;
+	act.sa_flags = SA_SIGINFO;
 	act.sa_sigaction = handler;
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
